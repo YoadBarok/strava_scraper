adf48f229eedf3d42fd4963a4bd65037a7061d0f
YoadBarok <yoad.barok@statsperform.com>
2024-09-05 15:38:46 +0200

    debug based on env

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from blueprints.plotting import plotting_bp
@@ -5,6 +6,8 @@ from blueprints.auth import auth_bp
 from blueprints.activities import activities_bp
 
 load_dotenv()
+
+debug = os.getenv("ENV") == "development"
 
 app = Flask(__name__)
 
@@ -14,4 +17,4 @@ app.register_blueprint(activities_bp)
 
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=8080, debug=debug)
